2d4fbc6add45d8a12838436b0b5ec0d8b515f397
Mike Nieva <miguelnieva@conexionew.com>
2017-09-12 02:09:29 -0500

    Footer

--- footer.php
+++ footer.php
@@ -6,24 +6,19 @@
         </p>
             <ul class="icons">
                 <li>
-                    <a class="icon fa-facebook" style="cursor: pointer;">
+                    <a class="icon fa-facebook" style="cursor: pointer;" href="http://facebook.com/tabuniversity">
                         <span class="label">Facebook</span>
                     </a>
                 </li>
                 <li>
-                    <a class="icon fa-twitter" style="cursor: pointer;">
+                    <a class="icon fa-twitter" style="cursor: pointer;" href="http://twitter.com/tabuniversity_">
                         <span class="label">Twitter</span>
-                    </a>
-                </li>
-                <li>
-                    <a class="icon fa-linkedin" style="cursor: pointer;">
-                        <span class="label">LinkedIn</span>
                     </a>
                 </li>
             </ul>
     </section>
     <section class="narrow">
-    <h4>Menú</h4>
+    <h3>Menú</h3>
     <ul class="links">
         <li>
             <a href="http://tabuniversity.com/publicaciones" style="cursor: pointer;">Publicaciones</a>
@@ -32,16 +27,14 @@
             <a href="http://tabuniversity.com/eventos" style="cursor: pointer;">Eventos</a>
         </li>
         <li>
-            <a style="cursor: pointer;">Comunidad</a>
-        </li>
-        <li>
-            <a style="cursor: pointer;">Empresas</a>
+            <a href="http://facebook.com/groups/ComunidadTAB/" style="cursor: pointer;">Comunidad</a>
         </li>
     </ul>
 </section>
 <section>
     <h3>Contacto</h3>
-    <p style="color: black;">Insurgentes Sur 859 Piso 2 Suite 282.</p>
-    <p></p>
+    <p style="color: black;margin:0 0 1rem 0">Insurgentes Sur 859 Piso 2 Suite 282.</p>
+    <p style="color: black;margin:0 0 1rem 0">03810 | Nápoles | Benito Juárez</p>
+    <p style="color: black;margin:0 0 1rem 0">Ciudad de México. México.</p>
     <p><a class="major" style="cursor: pointer;">(+52) 155 3073 8220</a></p></section></div><div class="copyright"><p>
     © TAB University 2017. Una empresa de Grupo VERSE. Todos los derechos reservados.</p></div></footer>
